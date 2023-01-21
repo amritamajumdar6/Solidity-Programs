f6ba8dfaf7e4f758ff93c5eb83964f9337b0779e
Amrita Majumdar <102658028+amritamajumdar6@users.noreply.github.com>
2023-01-21 13:57:34 +0530

    Create Global Variables.sol

---
+++ Global Variables.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract GlobalVariables
+{
+    function MyglobalVariables() external view returns (address, uint, uint, bytes32)
+    {
+        // These are global variables provide information about the Blockchain.
+        address sender = msg.sender;
+        // address that called this function
+        uint timeStamp=block.timestamp;
+        // timestamp(in seconds) of current block.
+        uint blockNum = block.number;
+        // current block number
+        bytes32 blockHash = blockhash(block.number);
+        // hash of given block
+        // here we get the hash of the current block
+        // WARNING : only works for 256 recent blocks.
+        return (sender, timeStamp, blockNum, blockHash);
+    }
+
+    function returnSender() external view returns (address)
+    {
+        return msg.sender;
+    }
+}
