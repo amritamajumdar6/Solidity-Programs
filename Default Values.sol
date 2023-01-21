a6e2cf40571ffaf38a33e426d743f51cfa83f3c2
Amrita Majumdar <102658028+amritamajumdar6@users.noreply.github.com>
2023-01-21 13:59:47 +0530

    Create Default Values.sol

---
+++ Default Values.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract DefaultValues
+{
+    // These are all called state variables
+    int public MyIntegerValue; //0
+    uint public MyUintVariable; //0
+    bool public MyBooleanVariables; // false
+    address public MyDefaultAddress; //00
+    bytes32 public MyDefaultBytes32; //00
+    string public MyDefaultString; //null
+}
