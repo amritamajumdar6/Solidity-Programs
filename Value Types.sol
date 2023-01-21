75e285ce3d84239160437dfacf1fe0cf02cec6da
Amrita Majumdar <102658028+amritamajumdar6@users.noreply.github.com>
2023-01-21 13:54:30 +0530

    Create Value Types.sol

---
+++ Value Types.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.13;
+
+contract ValueTypes
+
+{
+
+    bool public Myboolean_variable=true;
+    int public MyInteger_variable=-123;
+    uint public MyUint_variable=123;
+    address public MyPublic_Address= 0xfC9Dfbb7A367cdd20e8F064c857d1B138Ebf9FC7;
+    bytes32 public Myhash = 0xcedb8aa19dcf2ba82a4bbbbb946792d9804ac19196c7c3cab1ab110a51084aaf;
+}
